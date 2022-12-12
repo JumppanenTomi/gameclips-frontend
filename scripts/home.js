4bf310e147033feebb020d6b1954d4938c58b293
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-12-12 21:36:16 +0200

    if there are no clip give message

--- scripts/home.js
+++ scripts/home.js
@@ -24,62 +24,68 @@ async function getQuery() {
 }
 
 getQuery().then(async function (clipsData) {
-    clipsData.forEach(clip => {
-        const article=document.createElement('article');
-        article.className="clip-post"
+    if (clipsData.length==0) {
+        const emptytext=document.createElement('h2');
+        emptytext.textContent="There are no clips. Start uplaoding them."
+        clips.appendChild(emptytext)
+    } else {
+        clipsData.forEach(clip => {
+            const article=document.createElement('article');
+            article.className="clip-post"
 
-        const title=document.createElement('h2');
-        title.textContent=clip.title
-        const desc=document.createElement('p');
-        desc.textContent=clip.description
+            const title=document.createElement('h2');
+            title.textContent=clip.title
+            const desc=document.createElement('p');
+            desc.textContent=clip.description
 
-        const poster=document.createElement('div');
-        poster.className="posted-by"
-        const posterImg=document.createElement('img');
-        posterImg.src="https://wallpapers.com/images/hd/cool-neon-blue-profile-picture-u9y9ydo971k9mdcf.jpg"
-        const posterName=document.createElement('label');
-        posterName.textContent=clip.username
+            const poster=document.createElement('div');
+            poster.className="posted-by"
+            const posterImg=document.createElement('img');
+            posterImg.src="https://wallpapers.com/images/hd/cool-neon-blue-profile-picture-u9y9ydo971k9mdcf.jpg"
+            const posterName=document.createElement('label');
+            posterName.textContent=clip.username
 
-        const clipEl=document.createElement('video');
-        clipEl.setAttribute('controls', '');
-        const clipSrc=document.createElement('source');
-        clipSrc.src=url+'/static/'+clip.url
-        clipSrc.setAttribute('type', 'video/mp4')
+            const clipEl=document.createElement('video');
+            clipEl.setAttribute('controls', '');
+            const clipSrc=document.createElement('source');
+            clipSrc.src=url+'/static/'+clip.url
+            clipSrc.setAttribute('type', 'video/mp4')
 
-        const textBox=document.createElement('input')
-        textBox.setAttribute('type', 'text')
-        textBox.setAttribute('id', 'comment-box')
-        textBox.setAttribute('placeholder', 'Write a comment')
+            const textBox=document.createElement('input')
+            textBox.setAttribute('type', 'text')
+            textBox.setAttribute('id', 'comment-box')
+            textBox.setAttribute('placeholder', 'Write a comment')
 
-        const btn=document.createElement('button')
-        btn.setAttribute('id', 'post')
-        btn.textContent='Send'
+            const btn=document.createElement('button')
+            btn.setAttribute('id', 'post')
+            btn.textContent='Send'
 
-        const collapse=document.createElement('ul')
-        collapse.className='collapse'
+            const collapse=document.createElement('ul')
+            collapse.className='collapse'
 
-        const li=document.createElement('li')
+            const li=document.createElement('li')
 
-        const toggle=document.createElement('div')
-        toggle.className='toggle'
+            const toggle=document.createElement('div')
+            toggle.className='toggle'
 
-        const unordered=document.createElement('ul')
-        unordered.className='unordered'
+            const unordered=document.createElement('ul')
+            unordered.className='unordered'
 
-        clips.appendChild(article)
-        article.appendChild(title)
-        article.appendChild(desc)
-        article.appendChild(poster)
-        poster.appendChild(posterImg)
-        poster.appendChild(posterName)
-        article.appendChild(clipEl)
-        clipEl.appendChild(clipSrc)
-        article.appendChild(textBox)
-        article.appendChild(btn)
-        article.appendChild(collapse)
-        collapse.appendChild(li)
-        li.appendChild(toggle)
-        li.appendChild(unordered)
-    });
+            clips.appendChild(article)
+            article.appendChild(title)
+            article.appendChild(desc)
+            article.appendChild(poster)
+            poster.appendChild(posterImg)
+            poster.appendChild(posterName)
+            article.appendChild(clipEl)
+            clipEl.appendChild(clipSrc)
+            article.appendChild(textBox)
+            article.appendChild(btn)
+            article.appendChild(collapse)
+            collapse.appendChild(li)
+            li.appendChild(toggle)
+            li.appendChild(unordered)
+        });
+    }
 });
 
