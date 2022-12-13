3ce7b972b667a269fa8420551438cb94b90ce71c
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2022-12-13 17:51:12 +0200

    you can now get games by its id

--- scripts/browse-game.js
+++ scripts/browse-game.js
@@ -1,28 +1,11 @@
 const clips=document.querySelector('main');
-const trending=document.querySelector('.trending-games');
 let myUsername;
 if (getCookie('user')!="") {
     myUsername=JSON.parse(getCookie('user')).username;
 }
+const urlParams=new URLSearchParams(window.location.search);
 
-trendingGames().then(async function (gamesdata) {
-    for (let i=0; i<=10; i++) {
-        const li=document.createElement('li')
-
-        const a=document.createElement('a')
-        a.setAttribute('href', './browse-game.html?gameId='+gamesdata[i].id)
-
-        const img=document.createElement('img')
-        img.src='https://cdn.cloudflare.steamstatic.com/steam/apps/'+gamesdata[i].id+'/header.jpg'
-        img.alt=gamesdata[i].name+' cover'
-        
-        trending.appendChild(li)
-        li.appendChild(a)
-        a.appendChild(img)
-    }
-});
-
-getQuery().then(async function (clipsData) {
+getByGameId(urlParams.get('gameId')).then(async function (clipsData) {
     if (clipsData.length==0) {
         const emptytext=document.createElement('h2');
         emptytext.textContent="There are no clips. Start uplaoding them."
--- scripts/data/getVideos.js
+++ scripts/data/getVideos.js
@@ -2,3 +2,8 @@ async function getQuery() {
     const response=await fetch(serverUrl()+'/clip/getRandomQuery');
     return response.json();
 }
+
+async function getByGameId(id) {
+    const response=await fetch(serverUrl()+'/clip/getByGameId/'+id);
+    return response.json();
+}
